f7b0cc5e7ec3359c05682f3c2c42983722bb757b
student <tomerbrown@mail.tau.ac.il>
2021-12-17 21:03:22 +0200

    added inside queue functions the locks to avoid simoultanous access

--- pfind.c
+++ pfind.c
@@ -15,11 +15,6 @@
 
 
 
-
-//-------------------------------------------------------------------------
-//                         Queue Related Functions
-//-------------------------------------------------------------------------
-
 /*A struct to represent a node in the queue
 Fields:
 1) void* value (Will be char* for directory queue and int* for thread queue)
@@ -29,17 +24,6 @@ typedef struct Node {
     struct Node* next;
     void* value;
 } Node;
-
-/*A function that initializes a node in the queue with dir_name value*/
-Node* init_Node (void* value){
-    Node* node = calloc(1 , sizeof(Node));
-    if (node == NULL){
-        return NULL;
-    }
-    node -> next = NULL;
-    node ->value = value;
-    return node;
-}
 
 /*A struct to represent a Queue
 Fields:
@@ -53,6 +37,46 @@ typedef struct Queue {
     Node* tail;
 } Queue;
 
+/* -----------------------------------------------------------------------------------------------
+-----------------------------------------    Global Variables -----------------------------------
+-------------------------------------------------------------------------------------------------*/
+
+char* term;
+int n;
+
+pthread_mutex_t all_initialized_lock;
+atomic_int initialized_counter = 0;
+pthread_cond_t all_initialized_cond;
+
+
+atomic_int error_threads = 0;
+
+pthread_mutex_t queue_lock;
+pthread_mutex_t thread_lock;
+pthread_cond_t empty_cond;
+
+Queue directory_queue;
+Queue threads_queue;
+
+
+//-------------------------------------------------------------------------
+//                         Queue Related Functions
+//-------------------------------------------------------------------------
+
+
+
+/*A function that initializes a node in the queue with dir_name value*/
+Node* init_Node (void* value){
+    Node* node = calloc(1 , sizeof(Node));
+    if (node == NULL){
+        return NULL;
+    }
+    node -> next = NULL;
+    node ->value = value;
+    return node;
+}
+
+
 /*A function that initializes a empty queue*/
 Queue init_Queue(){
     Queue* queue = calloc(1,sizeof(Queue));
@@ -64,9 +88,13 @@ Queue init_Queue(){
 
 /*Returns 1 if and only if the queue is empty (e.i. it's length is 0) */  
 int is_empty(Queue* q){
-    return ((q->len)==0);
+    pthread_mutex_lock(&queue_lock);
+    int x = (q->len)==0;
+    pthread_mutex_unlock(&queue_lock);
+    return x;
     //todo: Add locks to avoid collisions (Changes in queue in the middle of is_empty)
 }
+
 
 /*given a dir_name to enter to queue, the function inserts it as the last element and changes pointers accordingly*/
 int insert (Queue* q ,void* value){
@@ -74,6 +102,7 @@ int insert (Queue* q ,void* value){
     if (node == NULL){
         return PROBELM;
     }
+    pthread_mutex_lock(&queue_lock);
     if (q->len == 0){
         q->head = node;
         q->tail = node;
@@ -83,24 +112,22 @@ int insert (Queue* q ,void* value){
         q->tail = node;
     }
     q->len ++;
-    //todo: Lock neccessary parts to avoid collisions
+    pthread_mutex_unlock(&queue_lock);
     return SUCCESFULL;
 }
 
-/*Returns the first dir_name in the queue (by FIFO order) and removes it from queue*/
-Node* pull(Queue* q ){
-    
-    //assert it is not empty for debugging purposes
-    assert(!is_empty(q));
 
+/*Returns the first dir_name in the queue (by FIFO order) and removes it from queue*/
+Node* pull (Queue* q ){
+    pthread_mutex_lock(&queue_lock);
     //git the first one
     Node* node = q->head;
     //remove the first element and free it
     q->head = node->next;
     q->len--;
+    pthread_mutex_unlock(&queue_lock);
     return node;
 }
-
 
 /*Iterate throgh the queue and print it element by element*/
 void print_queue(Queue* q){
@@ -226,29 +253,54 @@ char* extend_path (char* original_path,char* extention){
 
 
 
-/* -----------------------------------------------------------------------------------------------
------------------------------------------    Global Variables -----------------------------------
--------------------------------------------------------------------------------------------------*/
-
-char* term;
-int n;
-
-pthread_mutex_t all_initialized_lock;
-atomic_int initialized_counter = 0;
-pthread_cond_t all_initialized_cond;
-
-
-atomic_int error_threads = 0;
-
-pthread_mutex_t queue_lock;
-pthread_cond_t empty_cond;
-
-Queue directory_queue;
-Queue threads_queue;
 
 /* -----------------------------------------------------------------------------------------------
 -----------------------------------------    Thread Code -----------------------------------------
 -------------------------------------------------------------------------------------------------*/
+
+int search_directory(char* dir_name){
+    struct stat info; // A struct to hold some data about file (linux fs api)
+    char* exteneded_path; // A string to hold the concatenated path
+    struct dirent* x; //could be either a file or a folder
+    DIR* dir = opendir(dir_name);
+    if (dir==NULL){
+        printf("Directory %s: Permission denied.\n", dir_name);
+        return PROBELM;
+    }
+    
+    x = readdir(dir);
+    while (x!=NULL){
+        if (strcmp(x->d_name,".")==0 || strcmp(x->d_name,"..")==0 || strcmp(x->d_name ,".git")==0){
+            //case it is . or .. ignore it
+            x = readdir(dir);
+            continue;
+        }
+        exteneded_path = extend_path(dir_name, x->d_name);
+        if (stat(exteneded_path,&info) < 0){
+            fprintf(stderr,"Error: can't get stat of directory entry for %s\n",exteneded_path);
+            return PROBELM;
+        }
+        if (!(info.st_mode & S_IRUSR) && (info.st_mode & S_IXUSR)){
+            printf("Directory %s: Permission denied.\n", exteneded_path);
+            return PROBELM;
+        }
+        if (S_ISDIR(info.st_mode)){
+            //printf("Folder: %s\n",exteneded_path);
+
+            insert(&directory_queue , exteneded_path);
+            pthread_cond_broadcast(&empty_cond);
+            
+        }
+        else if (name_contains_term(x->d_name,term)){
+            //if the filename contains the term -> print it and increment the counterS
+            printf("%s\n",exteneded_path);
+        }
+        x = readdir(dir);
+    }
+
+    return 0;
+}
+
 
 void* searching_thread (void* arg){
     int tid = *(int*)arg;
@@ -265,35 +317,33 @@ void* searching_thread (void* arg){
     int waiting_flag = 0;
     pthread_cond_broadcast(&all_initialized_cond);
     pthread_mutex_unlock(&all_initialized_lock);
-
-
     while (1){
         printf("Thread %d is running\n",tid);
-        pthread_mutex_lock(&queue_lock);
+        pthread_mutex_lock(&thread_lock);
         while (is_empty(&directory_queue)){
             if (waiting_flag == 0){
-                // If it is the first time it waits for the queue to get full mark it and insert it to queue.
+                // If it is the first time it waits for the queue to get full. mark it and insert it to queue.
                 waiting_flag = 1;
                 if (insert(&threads_queue, (void*) &tid)==PROBELM){
                     error_threads++;
                     fprintf(stderr,"Error in thread %d: Could not allocate memory properly\n",tid);
-                    pthread_mutex_unlock(&queue_lock);
+                    pthread_mutex_unlock(&thread_lock);
                     pthread_exit(NULL);
                 }
                 print_queue_int(&threads_queue);
             }
             if (threads_queue.len + error_threads == n && is_empty(&directory_queue)){
                 //If it is the end - tell everyone it is the end and finish
-                pthread_mutex_unlock(&queue_lock);
+                pthread_mutex_unlock(&thread_lock);
                 pthread_cond_broadcast(&empty_cond);
                 printf("Thread %d : Ending -Errors = %d | waiting = %d\n",tid ,error_threads,threads_queue.len);
                 return SUCCESFULL;
             }
-            pthread_cond_wait(&empty_cond, &queue_lock);
+            pthread_cond_wait(&empty_cond, &thread_lock);
         }
         //Now we know that the thread has job to do - and that it has the lock
         node = pull(&directory_queue);
-        pthread_mutex_unlock(&queue_lock);
+        pthread_mutex_unlock(&thread_lock);
         strcpy(dir_name,(char*) node->value);
         free(node);
         printf("Thread %d: dir_name = %s\n",tid,dir_name);
@@ -305,6 +355,9 @@ void* searching_thread (void* arg){
 /* -----------------------------------------------------------------------------------------------
 -------------------------------------------    Main   --------------------------------------------
 -------------------------------------------------------------------------------------------------*/
+
+
+
 
 int main(int argc, char* argv[]){
 
@@ -327,7 +380,9 @@ int main(int argc, char* argv[]){
     pthread_mutex_init(&all_initialized_lock, NULL);
     pthread_cond_init (&all_initialized_cond,NULL);
 
+
     pthread_mutex_init(&queue_lock, NULL);
+    pthread_mutex_init(&thread_lock, NULL);
     pthread_cond_init (&empty_cond, NULL);
     
     
@@ -365,7 +420,9 @@ int main(int argc, char* argv[]){
     pthread_cond_destroy(&all_initialized_cond);
 
     pthread_mutex_destroy(&queue_lock);
+    pthread_mutex_destroy(&thread_lock);
     pthread_cond_destroy(&empty_cond);
+
 
     return 0;
 }
